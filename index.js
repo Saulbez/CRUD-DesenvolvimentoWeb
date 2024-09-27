00d2e61b5b1d0fe7d15816271baed9d46dbb7268
Saulbez <148885604+Saulbez@users.noreply.github.com>
2024-09-27 00:55:01 -0300

    Login/Signup autenticação e slide
    Mudança no form para login/signup e lógica no backend para criação de conta, criptografia e autenticação.

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 import express from "express"
 import bodyParser from "body-parser"
+import bcrypt from "bcrypt"
 
 const app = express();
 const port = 3000;
+
+const users = [];
+
+app.use(express.json());
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -11,11 +16,36 @@ app.get("/", (req, res) => {
     res.render("login.ejs");
 });
 
-app.post("/login", (req, res) => {
-    if (req.body["usuario"] === "admin" && req.body["senha"] === "admin") {
-        res.render("index.ejs")
-    } else {
-        res.render("login.ejs", { mensagem: "Usuário ou senha inválidos" });
+app.post("/signup", async (req, res) => {
+    try {
+        const salt = await bcrypt.genSalt();
+        const hashedPassword = await bcrypt.hash(req.body.password, salt);
+        console.log(salt);
+        console.log(hashedPassword);
+        const user = { email: req.body.email, password: hashedPassword}
+        users.push(user);
+
+        res.render("login.ejs");
+        res.status(201);
+        
+    } catch (error) {
+        res.render("login.ejs");
+        res.status(500);
+    }
+});
+
+app.post("/login", async (req, res) => {
+    const user = users.find(user => user.email === req.body.email);
+    if (user == null) {
+        res.render("login.ejs", { mensagem: "Email ou senha inválido" });
+    } try {
+        if (await bcrypt.compare(req.body.password, user.password)) {
+            res.render("index.ejs");
+        } else {
+            res.render("login.ejs", { mensagem: "Email ou senha inválido" });
+        }
+    } catch (error) {
+        res.status(500);
     }
 });
 
